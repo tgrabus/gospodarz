cee7bba88e163e9198b21b6914cc72d9c68ed1ab
tgrabus <tgrabus@outlook.com>
2016-01-09 14:21:17 +0100

    delete ide related files

--- api/api/settings.py
+++ api/api/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'api.urls'
